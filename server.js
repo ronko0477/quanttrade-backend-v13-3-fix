0aebd0886847155847d6dc1ab8f4fbeb8fe85732
ronko0477 <ronny4777@t-online.de>
2026-03-31 10:48:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.json());
-const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ===== STATE =====
